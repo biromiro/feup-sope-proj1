9c60521254ea3a49300f6c25a3c3d27f8935ce93
Andre Moreira <andre.moreira.9@hotmail.com>
2021-03-04 18:48:44 +0000

    Add simple options parser

--- src/args_parser.c
+++ src/args_parser.c
@@ -1,9 +1,31 @@
 #include "args_parser.h"
 
+extern int opterr;
+
 int parse_args(cmd_args_t *args, int argc, char *argv[], char *envp[]) {
-    args->recursive = true;
-    args->verbose = false;
-    args->verbose_on_modidy = true;
-    printf("Ok!");
+    memset(args, 0, sizeof(cmd_args_t));
+    opterr = 0;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "vcR")) != EOF) {
+        switch (opt) {
+            case 'v':
+                args->verbose = true;
+                args->verbose_on_modify = false;
+                break;
+            case 'c':
+                args->verbose_on_modify = !args->verbose;
+                break;
+            case 'R':
+                args->recursive = true;
+                break;
+            case '?':
+                printf("Invalid flag!\n");
+                break;
+            default:
+                break;
+        }
+    }
+
     return 0;
 }
--- src/args_parser.h
+++ src/args_parser.h
@@ -3,11 +3,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 typedef struct cmd_args {
     bool verbose;
-    bool verbose_on_modidy;
+    bool verbose_on_modify;
     bool recursive;
+    mode_t mode;
     // Add modes and paths maybe
 } cmd_args_t;
 
