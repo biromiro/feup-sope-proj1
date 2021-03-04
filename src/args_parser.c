d055fad3a94dbb76c7e5e7bf73401e45bcf81acc
Andre Moreira <andre.moreira.9@hotmail.com>
2021-03-04 12:29:26 +0000

    Add some stubs and example files

---
+++ src/args_parser.c
@@ -0,0 +1,10 @@
+#include "args_parser.h"
+
+int parse_args(cmd_args_t *args, int argc, char *argv[], char *envp[])
+{
+    args->recursive = true;
+    args->verbose = false;
+    args->verbose_on_modidy = true;
+    printf("Ok!");
+    return 0;
+}
---
+++ src/args_parser.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct cmd_args
+{
+    bool verbose;
+    bool verbose_on_modidy;
+    bool recursive;
+    // Add modes and paths maybe
+} cmd_args_t;
+
+int parse_args(cmd_args_t *args, int argc, char *argv[], char *envp[]);
