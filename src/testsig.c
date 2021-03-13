d4012671f3228632448c0ce7ab6fcc81470dbce0
Andre Moreira <andre.moreira.9@hotmail.com>
2021-03-13 10:50:04 +0000

    Fix signals used

--- src/testsig.c
+++ src/testsig.c
@@ -22,44 +22,60 @@ char *cmdline[] = {"sleep", "3", (void *)0};
 char *newenviron[] = {"USER=Linus", NULL};
 
 static bool waiting = false;
+static pid_t id = -1;
 
-void clear_buffer() {
-    char c;
-    while (((c = getchar()) != EOF) && (c != '\n'))
-        printf("CHAR: %d\n\n", c);
+int get_clean_char() {
+    char c = getchar(), tmp;
+    while (((tmp = getchar()) != EOF) && (tmp != '\n'))
+        ;
+    return c;
+}
+
+typedef enum {
+    SEND,
+    RECEIVE,
+} pipe_protocol_t;
+
+void exchange_pipe(pipe_protocol_t stat) {
+    switch (stat) {
+        case SEND: {
+        }
+
+        break;
+
+        default:
+            break;
+    }
 }
 
 void sighandler(int signo) {
+    if (waiting) {
+        printf("Process killed\n");
+        exit(1);
+    }
     waiting = true;
 
-    printf("%d ; %s ; %d ; %d \n", getpid(), "random_file", 5, 4);
+    printf("%d ; %s ; %d ; %d \n", getpid(), id > 0 ? "random_file" : "child", 5, 4);
     if (getpid() == getpgrp()) {
+        exchange_pipe(RECEIVE);
         printf("Do you want to exit? (y/Y or n/N)\n");
-        char c = getchar();
-        clear_buffer(stdin);
+        char c = get_clean_char();
         printf("CHAR OBTAINED: %c\n", c);
         switch (toupper(c)) {
             case 'Y':
                 printf("SENT SIGNAL\n");
-                killpg(getpgrp(), SIGUSR1);
+                killpg(getpgrp(), SIGINT);
                 exit(1);
                 break;
             case 'N':
             default:
-                killpg(getpgrp(), SIGUSR2);
+                killpg(getpgrp(), SIGCONT);
                 break;
         }
     }
 }
 
-void confirm_kill(int signo) {
-    if (waiting) {
-        printf("Process killed\n");
-        exit(1);
-    }
-}
-
-void dummy() {
+void cont_processes() {
     if (waiting) {
         printf("Continue process\n");
         waiting = false;
@@ -83,28 +99,17 @@ void setup_handler() {
     if (sigemptyset(&smask) == -1)
         perror("mask");
 
-    new.sa_handler = confirm_kill;
+    new.sa_handler = cont_processes;
     new.sa_mask = smask;
     new.sa_flags = 0;
 
-    if (sigaction(SIGUSR1, &new, &old) == -1)
-        perror("sigaction");
-
-    if (sigemptyset(&smask) == -1)
-        perror("mask");
-
-    new.sa_handler = dummy;
-    new.sa_mask = smask;
-    new.sa_flags = 0;
-
-    if (sigaction(SIGUSR2, &new, &old) == -1)
+    if (sigaction(SIGCONT, &new, &old) == -1)
         perror("sigaction");
 }
 
 int main(int argc, char *argv[], char *envp[]) {
     setup_handler();
 
-    pid_t id;
     setbuf(stdout, NULL);  // to make sure printf() have no buffer so, nodelay!
 
     printf("\nPID: %d", getpid());
@@ -121,7 +126,7 @@ int main(int argc, char *argv[], char *envp[]) {
                 perror("open");
                 exit(3);
             }
-            sleep(3);
+            sleep(1);
             while (waiting) {
             }
 
