2ffae34d424ae411d253a4db2ae4ec4430ffd0a5
Nuno Alves <nyansword@gmail.com>
2021-03-19 00:38:52 +0000

    fix: was resetting sigchld handler

--- include/signals.h
+++ include/signals.h
@@ -5,7 +5,7 @@
 #define ERR_SIGEMPTYMASK 3
 
 #define NO_OVERRIDE_SIG(s)                                          \
-    (s != SIGUSR1 && s != SIGINT && s != SIGCONT && \
+    (s != SIGUSR1 && s != SIGINT && s != SIGCONT && s != SIGCHLD && \
      s != SIGKILL && s != SIGSTOP && s != 32 && s != 33)
 
 #include <stdbool.h>
